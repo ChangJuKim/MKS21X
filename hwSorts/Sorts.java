075c4b543fef2d7d2e35af31e4a844822d44bc4f
Chang Ju Kim <kimchangju@yahoo.com>
2016-01-05 22:50:39 -0500

    Find bug, finish bubbleSort

--- hwSorts/Sorts.java
+++ hwSorts/Sorts.java
@@ -114,7 +114,7 @@ public class Sorts{
 	int hold = 0;
 	//keeps track of where to stop looking
 	//essentially i is where j stops
-	for (int i = data.length - 2; i > 0; i--) {
+	for (int i = data.length - 1; i > 0; i--) {
 	    for (int j = 0; j < i; j++){
 		if (data[j] > data[j+1]) {
 		    hold = data[j];
